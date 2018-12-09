3d76616766547ab798dc0f599bfd3c16042d9e7f
Mariusz Zakrzewski <mzakrze1@gmail.com>
2018-12-09 10:37:12 +0100

    Basic NER usage

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := "poc"
 version := "0.1"
 
 scalaVersion := "2.12.8"
+
+// https://mvnrepository.com/artifact/org.apache.opennlp/opennlp-tools
+libraryDependencies += "org.apache.opennlp" % "opennlp-tools" % "1.9.0"
--- src/main/scala/pl/mzakrze/nlp/Application.scala
+++ src/main/scala/pl/mzakrze/nlp/Application.scala
@@ -1,9 +1,54 @@
 package pl.mzakrze.nlp
 
+import opennlp.tools.namefind.TokenNameFinderModel
+import opennlp.tools.tokenize.TokenizerME
+import opennlp.tools.tokenize.TokenizerModel
+import opennlp.tools.namefind.NameFinderME
+import java.io.{BufferedInputStream, FileInputStream, IOException}
+
+import opennlp.tools.util.Span
+
 object Application {
 
+  val text = scala.io.Source.fromInputStream(new FileInputStream("database/paragraphs/politics.txt")).mkString.replace("\n", " ")
+
+  val personModelFile = "database/en-ner-person.bin"
+  val tokenModelFile = "database/en-token.bin"
+
   def main(args: Array[String]) = {
-    println("Hello world")
+
+    println("Loading model ...")
+
+    val bis = new BufferedInputStream(new FileInputStream(personModelFile))
+
+    val model = new TokenNameFinderModel(bis)
+
+    println("Model loaded. Tokenizing ...")
+
+    val nameFinder = new NameFinderME(model)
+
+    val tokens = tokenize(text)
+
+    val nameSpans = nameFinder.find(tokens)
+
+    debug(nameSpans, tokens.toList)
+  }
+
+  def debug(spans: Iterable[Span], tokens: List[String]) = {
+    for(ns <- spans){
+      val substring = tokens.slice(ns.getStart, ns.getEnd).mkString(" ")
+      val prob = ns.getProb
+      val entityType = ns.getType
+      println("{" + substring + "} -> " + entityType + "(" + prob + ")")
+    }
+  }
+
+  @throws[IOException]
+  def tokenize(sentence: String): Array[String] = {
+    val bis = new BufferedInputStream(new FileInputStream(tokenModelFile))
+    val tokenModel = new TokenizerModel(bis)
+    val tokenizer = new TokenizerME(tokenModel)
+    tokenizer.tokenize(sentence)
   }
 
 }
